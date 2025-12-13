97deb3328e0fe83b4ac145bb12456b6b863a8fc9
waster <44037446+eversincee@users.noreply.github.com>
2025-12-13 23:37:54 +0300

    Implement tokenizing

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_TOKENS 64
+#define DELIMITER " "
+void **tokenize(char *line, char **tokens) {
+    int i = 0;
+    char *token;
+    
+    token = strtok(line, DELIMITER);
+
+    while (token != NULL && i < MAX_TOKENS) {
+        tokens[i] = token;
+        i++;
+        token = strtok(NULL, DELIMITER);
+    }
+    
+    tokens[i] = NULL;
+}
 
 int main() {
     printf("> ");
     fflush(stdout);
+
     char line[1024];
     fgets(line, sizeof(line), stdin);
+    
+    char *args[MAX_TOKENS + 1];
+    tokenize(line, args);
+
     return 0;
 }
