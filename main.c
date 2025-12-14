43cc271afd90cdb5461d784032918d62b7c2f3d9
waster <44037446+eversincee@users.noreply.github.com>
2025-12-14 16:36:44 +0300

    implement cd and exit builtins

--- main.c
+++ main.c
@@ -8,8 +8,15 @@
 
 void tokenize(char *line, char **tokens);
 
+int execute(char **args);
+
+int sh_cd(char **args);
+int sh_exit(char **args);
+
 int main() {
-    while (1) {
+    int status = 1;
+
+    while (status) {
         printf("> ");
         fflush(stdout);
 
@@ -23,25 +30,7 @@ int main() {
             continue;
         }
 
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-        
-        int status;
-        pid_t wpid;
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            execvp(args[0], args);
-            perror("execvp");
-            _exit(1);
-        } else if (pid < 0) {
-            perror("shell");
-        } else {
-            do {
-                wpid = waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
+        status = execute(args);
     }
 
     return 0;
@@ -60,4 +49,48 @@ void tokenize(char *line, char **tokens) {
     }
     
     tokens[i] = NULL;
+}
+
+int sh_cd(char **args) {
+    if (args[1] == NULL) {
+        perror("expected argument to cd");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("shell");
+        }
+    }
+
+    return 1;
+}
+
+int sh_exit(char **args) {
+    return 0;
+}
+
+int execute(char **args) {
+    if (strcmp(args[0], "cd") == 0) {
+        return sh_cd(args);
+    }
+
+    if (strcmp(args[0], "exit") == 0) {
+        return sh_exit(args);
+    }
+    
+    int status;
+    pid_t wpid;
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        execvp(args[0], args);
+        perror("execvp");
+        _exit(1);
+    } else if (pid < 0) {
+        perror("shell");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
